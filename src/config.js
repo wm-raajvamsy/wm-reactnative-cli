b34e49a356ed95ee5815bd3923985634471edf8f
bandhavya <bandhavya.vakiti@wavemaker.com>
2021-07-11 17:17:46 +0530

    Updated android build functionality in the cli

--- src/config.js
+++ src/config.js
@@ -1,7 +1,17 @@
+const path = require('path');
+const fs = require('fs-extra');
+
 module.exports = {
     src: '',
     buildType: '',
     logDirectory: '',
     outputDirectory: '',
-    metaData: {}
+    metaData: {},
+    setMetaInfo: function(src) {
+        console.log('into fn src ..' + src);
+        src = path.resolve(src) + '/';
+        const jsonPath = src + 'app.json';
+        let data = fs.readFileSync(jsonPath);
+        return JSON.parse(data);
+    }
 };
